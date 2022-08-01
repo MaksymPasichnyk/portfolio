08090cbad453500f05b61e5111ccbd5e5f9de273
Maksym Pasichnyk <maxpas2512@gmail.com>
2022-08-01 16:43:02 +0300

    fixed section styles

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,36 +1,58 @@
 import { useContext } from "react";
 import { ThemeContext } from "../context/Theme";
 
-
 // icons
-import { AiOutlineInstagram, AiFillInstagram, AiOutlineLinkedin, AiFillLinkedin, AiOutlineGithub} from "react-icons/ai"
+import {
+  AiOutlineInstagram,
+  AiOutlineLinkedin,
+  AiFillLinkedin,
+  AiOutlineGithub,
+} from "react-icons/ai";
 import { SiGmail } from "react-icons/si";
 
+const socialContacts = [
+  {
+    icon: <AiOutlineGithub />,
+    url: "https://github.com/MaksymPasichnyk",
+  },
+  {
+    icon: <AiOutlineLinkedin />,
+    url: "https://www.linkedin.com/in/maksym-pasichnyk-357802191/",
+  },
+  {
+    icon: <SiGmail />,
+    url: "mailto:maxpas2512@gmail.com",
+  },
+];
+
 export default function Footer() {
-	const social = [<AiOutlineInstagram/>, <AiOutlineLinkedin/>, <AiOutlineGithub/>, <SiGmail/>];
-	const { selectedTheme } = useContext(ThemeContext);
+  const { selectedTheme } = useContext(ThemeContext);
 
-	const footerStyles = {
-		backgroundColor: selectedTheme.colors.text,
-	}
+  const footerStyles = {
+    backgroundColor: selectedTheme.colors.text,
+  };
 
-	const navStyles = {
-		color: selectedTheme.colors.linkColor,
-	}
+  const navStyles = {
+    color: selectedTheme.colors.linkColor,
+  };
 
-	const elements = social.map((item, index) => (
-		<li key={index} className="socials__item">
-			<a className="socials__link">
-				{item}
-			</a>
-		</li>
-	))
+  const elements = socialContacts.map((item, index) => (
+    <li key={index} className="socials__item">
+      <a
+        href={item.url}
+        target="_blank"
+        className="socials__link"
+      >
+        {item.icon}
+      </a>
+    </li>
+  ));
 
-	return (
-		<footer style={footerStyles} className="footer">
-				<nav style={navStyles}  name="contacts" id="contacts" className="socials">
-					{elements}
-				</nav>
-		</footer>
-	)
+  return (
+    <footer style={footerStyles} className="footer">
+      <nav style={navStyles} name="contacts" id="contacts" className="socials">
+        {elements}
+      </nav>
+    </footer>
+  );
 }
